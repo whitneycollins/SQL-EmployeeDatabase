b00131874b4310c331a6c599718548129d42b899
Whitney Collins <whitneycollins@Whitneys-MacBook.local>
2019-06-22 18:54:06 -0700

    final commit

--- employee_db.sql
+++ employee_db.sql
@@ -1,6 +1,4 @@
-
 --Data Analysis
-
 --List the following details of each employee: employee number, last name, first name, gender, and salary.
 SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary
 FROM employees AS e
