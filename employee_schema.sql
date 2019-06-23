9f3cd7247379b2a948476de549bde62d7feea5de
Whitney Collins <whitneycollins@Whitneys-MacBook.local>
2019-06-23 11:26:26 -0700

    minor edit to db

--- employee_schema.sql
+++ employee_schema.sql
@@ -1,33 +1,18 @@
-﻿-- Exported from QuickDBD: https://www.quickdatatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/xRTAr9
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
+DROP TABLE "departments";
+DROP TABLE "employees";
+DROP TABLE "dept_emp";
+DROP TABLE "dept_manager";
+DROP TABLE "salaries";
+DROP TABLE "titles";
 
 CREATE TABLE "departments" (
     "dept_no" VARCHAR   NOT NULL,
     "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
-CREATE TABLE "dept_emp" (
-    "emp_no" INTEGER   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR   NOT NULL,
-    "emp_no" INTEGER   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no"
+	CONSTRAINT pk_departments PRIMARY KEY (
+    	dept_no
      )
 );
 
@@ -38,36 +23,48 @@ CREATE TABLE "employees" (
     "last_name" VARCHAR   NOT NULL,
     "gender" VARCHAR   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
+	CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
      )
+
 );
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
 
 CREATE TABLE "salaries" (
     "emp_no" INTEGER   NOT NULL,
     "salary" FLOAT   NOT NULL,
     "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
-     )
+    "to_date" DATE   NOT NULL
+    
 );
 
 CREATE TABLE "titles" (
     "emp_no" INTEGER   NOT NULL,
     "title" VARCHAR   NOT NULL,
     "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "emp_no"
-     )
+    "to_date" DATE   NOT NULL
 );
-
-ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "dept_emp" ("dept_no");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dep_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
@@ -75,9 +72,10 @@ REFERENCES "departments" ("dept_no");
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "titles" ("emp_no");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "salaries" ("emp_no");
+REFERENCES "emplyees" ("emp_no");
+
 
